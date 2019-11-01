88fc3927af8c48ebd7d7dbb84871b86e5b1def04
luthfikw <luthfi.ramadhan@koinworks.com>
2019-11-01 11:19:58 +0700

    update install librdkafka with brew

--- ubuntu/librdkafka.sh
+++ ubuntu/librdkafka.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-apt-get update
-apt-get install software-properties-common git -y
-git clone https://github.com/edenhill/librdkafka.git && \
-    cd librdkafka && \
-    ./configure --prefix /usr && \
-    make && \
-    make install && \
-    apt-get install pkg-config -y
-cd ..
-rm -R librdkafka
+if [ -z "$(which brew)" ]; then
+    ./brew.sh
+fi
+
+if [ -z "$(which brew)" ]; then
+    echo "failed to install brew"
+fi
+
+brew install librdkafka pkg-config
