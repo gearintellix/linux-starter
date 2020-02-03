42631fc58afe42cf19e8ed9782735f5842950eed
Luthfi GearIntellix <luthfigearintellix@hotmail.com>
2020-02-03 18:12:12 +0700

    update protobuf installer script

--- ubuntu/protobuf.sh
+++ ubuntu/protobuf.sh
@@ -4,6 +4,9 @@ PROTOBUF_VERSION="3.7.1"
 
 curl -OL https://github.com/google/protobuf/releases/download/v$PROTOBUF_VERSION/protoc-$PROTOBUF_VERSION-linux-x86_64.zip
 unzip protoc-$PROTOBUF_VERSION-linux-x86_64.zip -d _outp
+rm protoc-$PROTOBUF_VERSION-linux-x86_64.zip
+
+sudo cp -r _outp/bin/* /usr/local/bin/
 
 sudo mv _outp/include/* /usr/local/include/
 sudo chown -R $USER /usr/local/include/google
